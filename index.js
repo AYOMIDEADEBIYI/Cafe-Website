c2bb7e755c25d39daec30346b39341af40e7f31f
ADEBIYIAYOMIDE <temiayomi99@gmail.com>
2024-02-19 09:32:01 -0400

    navbar

---
+++ index.js
@@ -0,0 +1,23 @@
+function myBars() {
+    let bars = document.querySelector("#bar");
+    let nav = document.querySelector(".navigation");
+    bars.onclick = function () {
+        if (nav.style.right == "0%") {
+            nav.style.right = "-50%";
+            bars.src = "/images/menu.png"
+        } else {
+            nav.style.right = "0%";
+            bars.src = "/images/x.png"
+        }
+        nav.classList.toggle("new")
+    }
+
+}
+myBars()
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
